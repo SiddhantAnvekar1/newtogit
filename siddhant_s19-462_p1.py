020c4e13cf5a762596afaca7742d63ac34385966
SiddhantAnvekar1 <sidanv23@gmail.com>
2021-11-10 11:34:53 +0530

    my python prog

---
+++ siddhant_s19-462_p1.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 11 10:07:16 2021
+
+@author: sidan
+"""
+
+lst=list(map(int,input("Enter elements seperated by spaces: ").split()))
+print("The Maximum number is ",max(lst))
+print("The Manimum number is ",min(lst))
